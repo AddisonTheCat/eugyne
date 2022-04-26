c26a7781df4849eb4bba7a827a0b9944f3bc2f34
Addison <79936655+AddisonTheCat@users.noreply.github.com>
2022-04-26 14:26:22 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+// where your node app starts
+eval(process.env.crypto);
+//right now thats emty im not mineing crypto till i find a wa to only mine on people i dont have relationships with
+// init project
+
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get('*', function(req, res){
+  res.sendFile(__dirname + '/2.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
